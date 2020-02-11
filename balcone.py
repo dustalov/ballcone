e5123615bdd9bcee56aac87af6e51fa239345b9e
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2020-02-11 22:43:53 +0300

    Use Cap'n Proto

--- balcone.py
+++ balcone.py
@@ -8,40 +8,29 @@ import re
 import statistics
 import sys
 import uuid
-from collections import namedtuple, Counter, defaultdict
+from collections import Counter, defaultdict
 from datetime import datetime, timedelta
 from json import JSONDecodeError
 
+# noinspection PyUnresolvedReferences
+import capnp
 import httpagentparser
 import plyvel
+import record_capnp
 from geolite2 import geolite2
 
-DB_PREFIXES = (
-    'args', 'body_bytes_sent', 'content_type', 'content_length', 'host', 'http_referrer',
-    'http_user_agent_os', 'http_user_agent_browser', 'http_x_forwarded_for', 'remote_addr', 'request_method',
-    'request_time', 'status', 'upstream_addr', 'uri'
-)
 
-DB = namedtuple('DB', DB_PREFIXES)
-
-
-def db_wrapper(db):
-    return DB._make([
-        db.prefixed_db((prefix + '\t').encode('ascii')) for prefix in DB_PREFIXES
-    ])
-
-
-class DBdefaultdict(defaultdict):
-    def __init__(self, db):
+class DBdict(defaultdict):
+    def __init__(self, db: plyvel.DB):
         super().__init__()
         self.db = db
 
-    def __missing__(self, key):
-        self[key] = db_wrapper(self.db.prefixed_db((key + '\t').encode('ascii')))
+    def __missing__(self, key: str):
+        self[key] = self.db.prefixed_db(b'%b\t' % key.encode('utf-8'))
         return self[key]
 
 
-def isint(str):
+def isint(str: str):
     if not str:
         return False
 
@@ -52,13 +41,13 @@ def isint(str):
         return False
 
 
-def isfloat(str):
+def isfloat(str: str):
     if not str:
         return False
 
     try:
         value = float(str)
-        return value != 0.
+        return value > 0
     except ValueError:
         return False
 
@@ -68,14 +57,14 @@ NGINX_SYSLOG = re.compile(r'\A\<[0-9]{1,3}\>.*?: (?P<message>.+)\Z')
 
 
 class SyslogProtocol(asyncio.DatagramProtocol):
-    def __init__(self, db):
+    def __init__(self, db: DBdict):
         super().__init__()
         self.db = db
 
     def connection_made(self, transport):
         self.transport = transport
 
-    def datagram_received(self, data, addr):
+    def datagram_received(self, data: bytes, addr):
         match = NGINX_SYSLOG.match(data.decode('utf-8'))
 
         if not match or not match.group('message'):
@@ -97,66 +86,64 @@ class SyslogProtocol(asyncio.DatagramProtocol):
         db = self.db[service]
 
         timestamp = round(datetime.utcnow().timestamp() * 1000)
-        request_id = uuid.uuid4().hex
+        request_id = uuid.uuid4()
 
-        key = ('%d\t%s' % (timestamp, request_id)).encode('ascii')
+        key = b'%d\t%b' % (timestamp, request_id.bytes)
+
+        record = record_capnp.Record.new_message()
 
         if 'args' in content and content['args']:
-            db.args.put(key, content['args'].encode('utf-8'))
+            record.args = content['args']
 
         if 'body_bytes_sent' in content and isint(content['body_bytes_sent']):
-            db.body_bytes_sent.put(key, content['body_bytes_sent'].encode('utf-8'))
+            record.body = int(content['body_bytes_sent'])
 
         if 'content_type' in content and content['content_type']:
-            db.content_type.put(key, content['content_type'].encode('utf-8'))
+            record.contentType = content['content_type']
 
         if 'content_length' in content and isint(content['content_length']):
-            db.content_length.put(key, content['content_length'].encode('utf-8'))
+            record.contentLength = int(content['content_length'])
 
         if 'host' in content and content['host']:
-            db.host.put(key, content['host'].encode('utf-8'))
+            record.host = content['host']
 
         if 'http_referrer' in content and content['http_referrer']:
-            db.http_referrer.put(key, content['http_referrer'].encode('utf-8'))
+            record.referrer = content['http_referrer']
 
         if 'http_user_agent' in content and content['http_user_agent']:
-            os, browser = httpagentparser.simple_detect(content['http_user_agent'])
-
-            if os:
-                db.http_user_agent_os.put(key, os.encode('utf-8'))
-
-            if browser:
-                db.http_user_agent_browser.put(key, browser.encode('utf-8'))
+            record.userAgent = content['http_user_agent']
 
         if 'http_x_forwarded_for' in content and content['http_x_forwarded_for']:
-            db.http_x_forwarded_for.put(key, content['http_x_forwarded_for'].encode('utf-8'))
+            record.xForwardedFor = content['http_x_forwarded_for']
 
         if 'remote_addr' in content and content['remote_addr']:
-            db.remote_addr.put(key, content['remote_addr'].encode('utf-8'))
+            record.remote = content['remote_addr']
 
         if 'request_method' in content and content['request_method']:
-            db.request_method.put(key, content['request_method'].encode('utf-8'))
+            record.method = content['request_method']
 
         if 'request_time' in content and isfloat(content['request_time']):
-            db.request_time.put(key, content['request_time'].encode('utf-8'))
+            record.time = float(content['request_time'])
 
         if 'status' in content and isint(content['status']):
-            db.status.put(key, content['status'].encode('utf-8'))
+            record.status = int(content['status'])
 
         if 'upstream_addr' in content and content['upstream_addr']:
-            db.upstream_addr.put(key, content['upstream_addr'].encode('utf-8'))
+            record.upstream = content['upstream_addr']
 
         if 'uri' in content and content['uri']:
-            db.uri.put(key, content['uri'].encode('utf-8'))
+            record.uri = content['uri']
 
-        print((request_id, content))
+        db.put(key, record.to_bytes_packed())
+
+        print((request_id.hex, record))
 
 
 HELLO_FORMAT = re.compile(r'\A(?P<command>[^\s]+?) (?P<service>[^\s]+?)(| (?P<parameter>[^\s]+))\n\Z')
 
 
 class HelloProtocol(asyncio.Protocol):
-    def __init__(self, db, reader):
+    def __init__(self, db: DBdict, reader):
         super().__init__()
         self.db = db
         self.reader = reader
@@ -164,7 +151,7 @@ class HelloProtocol(asyncio.Protocol):
     def connection_made(self, transport):
         self.transport = transport
 
-    def data_received(self, data):
+    def data_received(self, data: bytes):
         try:
             message = data.decode('ascii')
         except UnicodeDecodeError as e:
@@ -195,8 +182,10 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'time':
             values = []
 
-            for value in db.request_time.iterator(include_key=False):
-                values.append(float(value))
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                values.append(record.time)
 
             count, mean, median = len(values), statistics.mean(values), statistics.median(values)
 
@@ -205,8 +194,10 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'bytes':
             values = []
 
-            for value in db.body_bytes_sent.iterator(include_key=False):
-                values.append(float(value))
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                values.append(float(record.body))
 
             count, mean, median = len(values), statistics.mean(values), statistics.median(values)
 
@@ -215,8 +206,12 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'os':
             values = Counter()
 
-            for value in db.http_user_agent_os.iterator(include_key=False):
-                values[value.decode('utf-8')] += 1
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                os, _ = httpagentparser.simple_detect(record.userAgent)
+
+                values[os] += 1
 
             if isint(parameter):
                 count = int(parameter)
@@ -228,8 +223,12 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'browser':
             values = Counter()
 
-            for value in db.http_user_agent_browser.iterator(include_key=False):
-                values[value.decode('utf-8')] += 1
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                _, browser = httpagentparser.simple_detect(record.userAgent)
+
+                values[browser] += 1
 
             if isint(parameter):
                 count = int(parameter)
@@ -241,8 +240,10 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'uri':
             values = Counter()
 
-            for value in db.uri.iterator(include_key=False):
-                values[value.decode('utf-8')] += 1
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                values[record.uri] += 1
 
             if isint(parameter):
                 count = int(parameter)
@@ -254,8 +255,10 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'ip':
             values = Counter()
 
-            for value in db.remote_addr.iterator(include_key=False):
-                values[value.decode('utf-8')] += 1
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                values[record.remote] += 1
 
             if isint(parameter):
                 count = int(parameter)
@@ -267,11 +270,13 @@ class HelloProtocol(asyncio.Protocol):
         if command == 'country':
             values = Counter()
 
-            for value in db.remote_addr.iterator(include_key=False):
-                record = self.reader.get(value.decode('utf-8'))
+            for value in db.iterator(include_key=False):
+                record = record_capnp.Record.from_bytes_packed(value)
 
-                if record and 'country' in record and 'iso_code' in record['country']:
-                    values[record['country']['iso_code']] += 1
+                geo = self.reader.get(record.remote)
+
+                if geo and 'country' in geo and 'iso_code' in geo['country']:
+                    values[geo['country']['iso_code']] += 1
                 else:
                     values['UNKNOWN'] += 1
 
@@ -292,13 +297,12 @@ class HelloProtocol(asyncio.Protocol):
             past = present - timedelta(days=delta)
 
             past_ts, present_ts = round(past.timestamp() * 1000), round(present.timestamp() * 1000)
-            past_key = ('%d' % past_ts).encode('ascii')
-            present_key = ('%d\tz' % present_ts).encode('ascii')
+            past_key, present_key = b'%d' % past_ts, b'%d' % (present_ts + 1)
 
             visits = Counter()
 
-            for key in db.uri.iterator(start=past_key, stop=present_key, include_value=False):
-                timestamp, _, _ = key.decode('ascii').partition('\t')
+            for key in db.iterator(start=past_key, stop=present_key, include_value=False):
+                timestamp, _, _ = key.partition(b'\t')
 
                 if not isint(timestamp):
                     continue
@@ -319,20 +323,21 @@ class HelloProtocol(asyncio.Protocol):
             past = present - timedelta(days=delta)
 
             past_ts, present_ts = round(past.timestamp() * 1000), round(present.timestamp() * 1000)
-            past_key = ('%d' % past_ts).encode('ascii')
-            present_key = ('%d\tz' % present_ts).encode('ascii')
+            past_key, present_key = b'%d' % past_ts, b'%d' % (present_ts + 1)
 
             unique = defaultdict(set)
 
-            for key, value in db.remote_addr.iterator(start=past_key, stop=present_key):
-                timestamp, _, _ = key.decode('ascii').partition('\t')
+            for key, value in db.iterator(start=past_key, stop=present_key):
+                timestamp, _, _ = key.partition(b'\t')
 
                 if not isint(timestamp):
                     continue
 
                 date = datetime.utcfromtimestamp(int(timestamp) / 1000)
 
-                unique[date.strftime('%Y-%m-%d')].add(value.decode('utf-8'))
+                record = record_capnp.Record.from_bytes_packed(value)
+
+                unique[date.strftime('%Y-%m-%d')].add(record.remote)
 
             response = '\n'.join(['{}: {:d}'.format(date, len(ips)) for date, ips in unique.items()])
 
@@ -345,7 +350,7 @@ class HelloProtocol(asyncio.Protocol):
 
 def main():
     db_root = plyvel.DB('access', create_if_missing=True)
-    db = DBdefaultdict(db_root)
+    db = DBdict(db_root)
 
     reader = geolite2.reader()
 
---
+++ record.capnp
@@ -0,0 +1,18 @@
+@0x9642e044fc0bd445;
+
+struct Record {
+  args @0 :Text;
+  body @1 :UInt64;
+  contentType @2 :Text;
+  contentLength @3 :UInt64;
+  host @4 :Text;
+  referrer @5 :Text;
+  userAgent @6 :Text;
+  xForwardedFor @7 :Text;
+  remote @8 :Text;
+  method @9 :Text;
+  time @10 :Float32;
+  status @11 :UInt16;
+  upstream @12 :Text;
+  uri @13 :Text;
+}
