22b4dcc946f827c33ff7bb2112e842d450835373
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2022-09-27 00:15:07 +0200

    Add duck-upgrade.sh

---
+++ tools/duck-upgrade.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -eu
+
+if [ "$#" -ne 4 ] ; then
+  echo "Usage: $(basename "$0") duckdb-cli-old duckdb-cli-new database-old database-new"
+  exit 1
+fi
+
+OLD=$1
+NEW=$2
+OLDDB=$3
+NEWDB=$4
+
+$OLD --version >/dev/null
+$NEW --version >/dev/null
+
+TEMP=$(mktemp -d) || exit 2
+trap 'rm -rf "$TEMP"' EXIT
+
+set -x
+
+$OLD "$OLDDB" -c "EXPORT DATABASE '$TEMP' (FORMAT PARQUET);"
+$NEW "$NEWDB" -c "IMPORT DATABASE '$TEMP';"
